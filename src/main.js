19bb1d969c73658677c238c1324d38a63eefad0e
DESKTOP-HIHUF8G\doluc <doluche.avalanche@gmail.com>
2022-03-05 16:08:55 +0900

    lesson6

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
 import router from "./router";
+import store from "./store/index";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
