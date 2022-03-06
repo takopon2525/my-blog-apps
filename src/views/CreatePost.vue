9f752c704c7bb14f93e66d4300898e30cf96b377
DESKTOP-HIHUF8G\doluc <doluche.avalanche@gmail.com>
2022-03-06 17:24:24 +0900

    vue-quill-editorの導入

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,1 +1,24 @@
-<template></template>
+<script setup>
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import { ref, watchEffect } from "vue";
+
+const content = ref(null);
+
+watchEffect(() => {
+  if (content.value) {
+    console.log("content.value", content.value);
+    // ここで情報をセットする
+  }
+});
+</script>
+<template>
+  <div class="mx-auto w-4/5">
+    <QuillEditor
+      v-model:content="content"
+      contentType="html"
+      theme="snow"
+      toolbar="full"
+    />
+  </div>
+</template>
